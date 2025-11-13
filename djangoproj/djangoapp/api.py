56a18445f4cbd3fe84e98632f6e202916eee3664
Rom <romitrajbind0@gmail.com>
2025-11-13 22:27:32 +0530

    daphne added on romits cum-puter

--- djangoproj/djangoapp/api.py
+++
@@ -1,6 +0,0 @@
-from ninja import NinjaAPI
-api = NinjaAPI()
-
-@api.get("/hello")
-async def hello(request):
-    return {"message": "hello"}
--- djangoproj/djangoproj/urls.py
+++ djangoproj/djangoproj/urls.py
@@ -17,9 +17,7 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
-from djangoapp.api import api
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/", api.urls)
 ]
