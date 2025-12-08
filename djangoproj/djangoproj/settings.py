1eff591c4cc8590932945717e5babcde836266a7
Rom <romitrajbind0@gmail.com>
2025-12-08 15:56:03 +0530

    adding stuff

--- djangoproj/djangoproj/settings.py
+++ djangoproj/djangoproj/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -73,6 +74,11 @@ TEMPLATES = [
     },
 ]
 
+
+CORS_ALLOWED_ORIGINS = [
+    "https://localhost:3000",
+    "https://127.0.0.1:3000",
+]
 # WSGI_APPLICATION = "djangoproj.wsgi.application"
 ASGI_APPLICATION = "djangoproj.asgi.application"
 
