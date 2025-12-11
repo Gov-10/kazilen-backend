2488fa5fad27aee103fd37717ac240b95dee433f
Rom <romitrajbind0@gmail.com>
2025-12-11 18:58:16 +0530

    !Automated message! Did work at Thu Dec 11 06:58:16 PM IST 2025

--- djangoproj/djangoapp/api.py
+++ djangoproj/djangoapp/api.py
@@ -1,6 +1,12 @@
+from typing_extensions import List
+from typing import List
 from ninja import NinjaAPI
+from django.shortcuts import get_object_or_404
 from .models import Customer, Worker, History
 from .schemas import CustomerSchema, WorkerSchema, HistorySchema
 api = NinjaAPI()
 
-
+@api.get("/worker", response=List[WorkerSchema])
+def getAllWorker(request):
+    #workers = get_object_or_404(**request)
+    workers = Worker.objects.all()
