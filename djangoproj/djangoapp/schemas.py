bcabd87fcb48792fd11037b672942df9729feb1d
Rom <romitrajbind0@gmail.com>
2025-12-11 19:09:50 +0530

    !Automated message! Did work at Thu Dec 11 07:09:50 PM IST 2025

--- djangoproj/djangoapp/schemas.py
+++ djangoproj/djangoapp/schemas.py
@@ -2,11 +2,13 @@ from ninja import ModelSchema
 from .models import Customer, Worker, History
 
 class CustomerSchema(ModelSchema):
+    phoneNo: str
     class Meta:
         model = Customer
         fields = '__all__'
 
 class WorkerSchema(ModelSchema):
+    phoneNo: str
     class Meta:
         model = Worker
         fields = '__all__'
