20d8f964346ac085718dedc4a0b4f2694f06dc34
Rom <romitrajbind0@gmail.com>
2025-12-15 18:20:52 +0530

    !Automated message! Did work at Mon Dec 15 06:20:52 PM IST 2025

--- djangoproj/djangoapp/schemas.py
+++ djangoproj/djangoapp/schemas.py
@@ -1,6 +1,11 @@
-from typing import List
+from typing import List, Literal
 from ninja import ModelSchema, Schema
+from pydantic import ConfigDict
 from .models import Customer, Worker, History
+
+jobIdentifier = Literal[
+    "vehicle", "health", "carpenter", "electrician", "home", "appliance", "labour"
+]
 
 
 class CustomerSchema(ModelSchema):
@@ -9,14 +14,22 @@ class CustomerSchema(ModelSchema):
         fields = "__all__"
 
 
-class WorkerSchema(Schema):
-    id: int
-    name: str
-    jobProfile: str
-    address: str
-    phoneNo: List(int)
-    description: str
+#class WorkerSchema(Schema):
+#    id: int
+#    name: str
+#    jobProfile: jobIdentifier
+#    address: str
+#    phoneNo: List[int]
+#    description: str
+#    model_config = ConfigDict(from_attributes=True, arbitrary_types_allowed=True)
 
+class WorkerSchema(ModelSchema):
+    model_config = ConfigDict(
+            from_attributes=True,
+            model = Worker,
+            arbitrary_types_allowed=True,
+            fields = '__all__',
+            )
 
 class HistorySchema(ModelSchema):
     class Meta:
