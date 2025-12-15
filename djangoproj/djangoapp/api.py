4a4329c4465589f69c08d7b3f880360c9b66d7ee
Rom <romitrajbind0@gmail.com>
2025-12-15 17:46:09 +0530

    !Automated message! Did work at Mon Dec 15 05:46:09 PM IST 2025

--- djangoproj/djangoapp/api.py
+++ djangoproj/djangoapp/api.py
@@ -4,14 +4,32 @@ from ninja import NinjaAPI
 from django.shortcuts import get_object_or_404
 from .models import Customer, Worker, History
 from .schemas import CustomerSchema, WorkerSchema, HistorySchema
+
 api = NinjaAPI()
+
 
 @api.get("/workers", response=List[WorkerSchema])
 def getAllWorker(request):
     return Worker.objects.all()
 
+
 @api.get("/worker/{name}", response=WorkerSchema)
 def getSingleWorker(request, name: str):
-        worker = get_object_or_404(Worker, name=name)
-        return worker
+    worker = get_object_or_404(Worker, name=name)
+    return worker
 
+
+@api.get("/getworkerList/{filer}/{value}", response=[WorkerSchema])
+def getWorkerList(request, filter: str, value: str):
+    match filter:
+        case "name":
+            workers = Worker.objects.get(name=value)
+        case "jobProfile":
+            workers = Worker.objects.get(jobProfile=value)
+        case "rating":
+            workers = Worker.objects.get(rating=value)
+        case "location":
+            workers = Worker.objects.get(location=value)
+        case _:
+            return 
+    return workers
