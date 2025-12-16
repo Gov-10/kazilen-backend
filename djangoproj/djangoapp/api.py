8b76cdba1fc1f6460b3f4471d6e55dd21c07e8a0
Rom <romitrajbind0@gmail.com>
2025-12-16 19:56:40 +0530

    !Automated message! Did work at Tue Dec 16 07:56:40 PM IST 2025

--- djangoproj/djangoapp/api.py
+++ djangoproj/djangoapp/api.py
@@ -1,7 +1,7 @@
 from django.db.models import Q, QuerySet
 from typing_extensions import List
 from typing import List, Optional
-from ninja import NinjaAPI, Query, Schema
+from ninja import FilterSchema, NinjaAPI, Query, Schema
 from django.shortcuts import get_object_or_404
 from .models import Customer, Worker, History
 from .schemas import CustomerSchema, WorkerSchema, HistorySchema
@@ -9,9 +9,16 @@ from .schemas import CustomerSchema, WorkerSchema, HistorySchema
 api = NinjaAPI()
 
 
-class workerFilter(Schema):
-    category: Optional[str] = None
-    subcategory: Optional[str] = None
+class workerFilter(FilterSchema):
+    category: Optional[List[str]] = None
+    subcategory: Optional[List[str]] = None
+
+    def filter_category(self, value: List[str]) -> Q:
+        category_value = value[0]
+        return Q(category=category_value)
+    def filter_subcategory(self, value: List[str]) -> Q:
+        subcategory_value = value[0]
+        return Q(subcategory__contains=subcategory_value)
 
 @api.get("/worker", response=List[WorkerSchema])
 def getAllWorker(request):
@@ -25,11 +32,11 @@ def getAllWorker(request):
 
 
 @api.get("/filterworker", response=List[WorkerSchema])
-def getFilterWorker(request, filter: Query[workerFilter]):
-    workers: QuerySet[Worker] = Worker.objects.all();
-    q = filter.get_filter_expression()
-    if filter.category:
-        workers = workers.filter(category=filter.category)
-    if filter.subcategory:
-        workers = workers.filter(subcategory=filter.subcategory)
+def getFilterWorker(request, filter: workerFilter = Query(...)):
+    workers = Worker.objects.all();
+    workers = filter.filter(workers)
+    # if filter.category:
+    #     workers = workers.filter(category=filter.category)
+    # if filter.subcategory:
+    #     workers = workers.filter(subcategory=filter.subcategory)
     return workers
