ae498dd48f47803c344b2198a5f98a5e8c5430ba
Rom <romitrajbind0@gmail.com>
2025-12-17 19:59:27 +0530

    !Automated message! Did work at Wed Dec 17 07:59:27 PM IST 2025

--- djangoproj/djangoapp/schemas.py
+++ djangoproj/djangoapp/schemas.py
@@ -3,6 +3,7 @@ from typing import List
 from ninja import Field, ModelSchema, Schema
 from .models import Customer, Worker, History
 from pydantic_extra_types.phone_numbers import PhoneNumber
+from pydantic import field_validator
 
 class CustomerSchema(ModelSchema):
     class Meta:
@@ -16,7 +17,12 @@ class WorkerSchema(ModelSchema):
     class Meta:
         model = Worker
         fields = "__all__"
-
+    
+    @staticmethod
+    def resolve_phoneNo(obj):
+        if not obj.phoneNo:
+            return None
+        return str(obj.phoneNo)
 #class WorkerSchema(Schema):
 #    name: str
 #    address: str
