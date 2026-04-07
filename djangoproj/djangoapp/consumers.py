14dedadf127533561bb23988e4fa6be7f770cb29
Rom <romitrajbind0@gmail.com>
2026-04-07 21:08:35 +0530

    !Automated message! Did work at Tue Apr  7 09:08:35 PM IST 2026

--- djangoproj/djangoapp/consumers.py
+++ djangoproj/djangoapp/consumers.py
@@ -2,10 +2,12 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 import json
 class WorkerStatusConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        await self.channel_layer.group_add("workers", self.channel_name)
+        self.user = self.scope["user"]
+        self.group_name = f"user_{self.user.phoneNo}"
+        await self.channel_layer.group_add(self.group_name, self.channel_name)
         await self.accept()
     async def disconnect(self):
-        await self.channel_layer.group_discard("workers", self.channel_name)
+        await self.channel_layer.group_discard(self.group_name ,self.channel_name)
     async def worker_update(self, event):
         await self.send(text_data=json.dumps(event["data"]))
 
